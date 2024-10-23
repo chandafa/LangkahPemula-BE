f9fef59debc55fa99ea68e8166825e13384e12ce
chandafa <130040496+chandafa@users.noreply.github.com>
2024-10-23 14:51:04 +0700

    Update services.php

--- config/services.php
+++ config/services.php
@@ -14,21 +14,30 @@ return [
     |
     */
 
+    // You can remove this section if not using Mailgun
+    /*
     'mailgun' => [
         'domain' => env('MAILGUN_DOMAIN'),
         'secret' => env('MAILGUN_SECRET'),
         'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
         'scheme' => 'https',
     ],
+    */
 
+    // You can remove this section if not using Postmark
+    /*
     'postmark' => [
         'token' => env('POSTMARK_TOKEN'),
     ],
+    */
 
+    // You can remove this section if not using AWS SES
+    /*
     'ses' => [
         'key' => env('AWS_ACCESS_KEY_ID'),
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
+    */
 
 ];
